72e0c2632297039227854d7359cdaa6b45f11699
Ivan Bilotserkivskiy <ivanbilotserkivskiy@gmail.com>
2023-02-16 15:16:20 +0200

    set up React Router and Context hook

--- picture/src/App.js
+++ picture/src/App.js
@@ -2,11 +2,21 @@ import React from "react"
 import Header from "./components/Header"
 import Cart from "./pages/Cart"
 import Photos from "./pages/Photos"
+import {Routes, Route} from "react-router-dom"
 
 function App() {    
+
+
+
     return (
         <div>
-            <Header />
+               <Header/>
+            <Routes>
+                <Route exact path="/" element={<Photos/>}/>
+    
+                <Route  path="/cart" element={<Cart/>}/>
+            </Routes>
+         
             <h1>Home Page</h1>
         </div>
     )
--- picture/src/index.js
+++ picture/src/index.js
@@ -1,12 +1,16 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
 import './index.css'
+import { BrowserRouter as Router} from "react-router-dom"
+import { ContextProvider } from "./Context"
 
 import App from "./App.js"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <ContextProvider>
+  <Router>
     <App />
-  </React.StrictMode>
+  </Router>
+  </ContextProvider>
 );
