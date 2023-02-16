c27fbffb44305c591ea8223f3268716eeb0febc3
Ivan Bilotserkivskiy <ivanbilotserkivskiy@gmail.com>
2023-02-16 18:04:49 +0200

    image hover icons

--- picture/src/components/Image.js
+++ picture/src/components/Image.js
@@ -1,10 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
+
 
 function Image({className,img}) {
-   
+   const [hovered, setHovered] = useState(false)
+   const [icon, setIcon] = useState(false)
+   const heartIcon = hovered && <i onMouseEnter={()=>setIcon(true)} onMouseLeave={()=>setIcon(false)} className="ri-heart-line favorite"></i>
+   const cartIcon = hovered && <i  onMouseEnter={()=>setIcon(true)} onMouseLeave={()=>setIcon(false)} className="ri-add-circle-line cart"></i>
+   console.log(icon)
     return (
         <div className={`${className} image-container`}>
-            <img alt={img.url} src={img.url} className="image-grid"/>
+            <img onMouseEnter={()=>setHovered(true)} onMouseLeave={()=>!icon&&setHovered(false)} alt={img.url} src={img.url} className="image-grid"/>
+            {heartIcon}
+            {cartIcon}
         </div>
     )
 }
